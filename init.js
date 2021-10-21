9e2fded7a25e98bc266d1ef4c178c0cbec51f8fb
SakuraRoad <754483487@qq.com>
2021-10-21 14:28:05 +0800

    调整细节

--- init.js
+++
@@ -1,7 +0,0 @@
-let fs = require('fs');
-let root=__dirname
-let path = `module.exports="${process.cwd()}"`;
-path = path.replace(new RegExp(/\\/, "g"), "/");
-
-//console.log('root=',root,'path=',path)
-//fs.writeFileSync(root, path, 'UTF-8') ;
